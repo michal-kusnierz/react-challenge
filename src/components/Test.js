71e4d3b2b2534509fcc2891d1d5ecd4bcf57750b
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-19 11:46:51 +0100

    added react-async module with first test

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,8 +1,44 @@
 import React from "react";
-import { Segment } from "semantic-ui-react";
+// import { Segment } from "semantic-ui-react";
+import Async from 'react-async';
+import { fetchData } from "../services/fetchService";
 
-const Test = () => (
-  <Segment className="test">test field</Segment>
-);
+// const Test = () => (
+//   <Segment className="test">test field</Segment>
+// );
+
+
+
+const Test = () => {
+  return (
+    <div className="container">
+      <Async promiseFn={fetchData}>
+        {({ data, err, isLoading }) => {
+          if (isLoading) return "Loading..."
+          if (err) return `Something went wrong: ${err.message}`
+
+          if (data)
+            return (
+              <div>
+                <div>
+                  <h2>React Async - Random Users</h2>
+                </div>
+                {data.map(user=> (
+                  <div key={user.username} className="row">
+                    <div className="col-md-12">
+                      <p>{user.name}</p>
+                      <p>{user.email}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )
+        }}
+      </Async>
+    </div>
+  );
+}
+
+
 
 export default Test
---
+++ src/services/fetchService.js
@@ -0,0 +1,7 @@
+export const fetchData = () =>
+  // fetch("https://my12.digitalexperience.ibm.com/api/859f2008-a40a-4b92-afd0-24bb44d10124/delivery/v1/content/fa9519d5-0363-4b8d-8e1f-627d802c08a8")
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(res => res.json())
+
+
