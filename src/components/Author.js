c925e7e13eeaef8eed4bbf80f63da89388cda7fc
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-19 12:38:39 +0100

    beautify Author component

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,23 +1,25 @@
 import React from "react";
-import Async from 'react-async';
+import Async from "react-async";
 import { fetchData } from "../services/fetchService";
 import { Segment } from "semantic-ui-react";
 
 const Author = () => (
   <Segment className="author">
-        <Async promiseFn={fetchData}>
+    <Async promiseFn={fetchData}>
       {({ data, err, isLoading }) => {
         if (isLoading) return "Loading...";
         if (err) return `Something went wrong: ${err.message}`;
 
         if (data);
-        const authorData = JSON.parse(JSON.stringify(data.elements.author.value));
+        const authorData = JSON.parse(
+          JSON.stringify(data.elements.author.value)
+        );
 
-          return (
-            <div>
-              <p>by: {authorData}</p>
-            </div>
-          );
+        return (
+          <div>
+            <p>by: {authorData}</p>
+          </div>
+        );
       }}
     </Async>
   </Segment>
