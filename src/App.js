67c386cc63f3bdbde4bf62b9f202cd32ac115390
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-19 11:18:38 +0100

    added Heading component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Heading from './components/Heading';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App_container">
+      <header className="App_header">
         <p>first try to fetch data</p>
       </header>
+      <main>
+        <Heading />
+      </main>
     </div>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+const Heading = () => (
+  <Header className="header">the heading comes here</Header>
+);
+
+export default Heading;
