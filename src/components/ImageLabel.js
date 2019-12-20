5e4c868f3fe5dcd6a299debb94280d774cfa7392
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-20 09:44:34 +0100

    erased much of styles from Label

--- src/components/ImageLabel.js
+++ src/components/ImageLabel.js
@@ -13,7 +13,7 @@ const ImageLabel = () => (
       const imageData = data.elements.mainImage.value;
       const leadImageCaption = imageData.leadImageCaption.value;
       return (
-        <Label className="image-label" size='tiny' attached='bottom right' basic content >
+        <Label className="image-label" content>
           {leadImageCaption}
         </Label>
       );
