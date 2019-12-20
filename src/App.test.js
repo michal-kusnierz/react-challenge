9a25871dacc80806a8c7955c2d46e2496c4ee601
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-20 14:46:32 +0100

    fixed initial test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders React challenge paragraph', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/React challenge/i);
   expect(linkElement).toBeInTheDocument();
 });
